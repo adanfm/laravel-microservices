4d28e0a98fb447bff5f289ba6c358e7d9cc9f713
Adan Felipe Medeiros <adan.grg@gmail.com>
2022-01-11 20:27:59 -0300

    Feat - Adicionado recurso para rodar testes de produção

--- tests/Prod/Models/Traits/UploadFilesProdTest.php
+++ tests/Prod/Models/Traits/UploadFilesProdTest.php
@@ -5,17 +5,18 @@ namespace Tests\Prod\Models\Traits;
 use Illuminate\Http\UploadedFile;
 use Tests\Stubs\Models\UploadFilesStub;
 use Tests\TestCase;
+use Tests\Traits\TestProd;
 use Tests\Traits\TestStorages;
 
-class UploadFilesUnitTest extends TestCase
+class UploadFilesProdTest extends TestCase
 {
-    use TestStorages;
-
+    use TestStorages, TestProd;
     private $obj;
 
     protected function setUp(): void
     {
         parent::setUp();
+        $this->skipTestIfNotProd();
         $this->obj = new UploadFilesStub();
         \Config::set('filesystems.default', 'gcs');
         $this->deleteAllFiles();
@@ -23,7 +24,6 @@ class UploadFilesUnitTest extends TestCase
 
     public function testUploadFile()
     {
-        \Storage::fake();
         $file = UploadedFile::fake()->create('video.mp4');
         $this->obj->uploadFile($file);
         \Storage::assertExists("1/{$file->hashName()}");
@@ -38,12 +38,27 @@ class UploadFilesUnitTest extends TestCase
         \Storage::assertExists("1/{$file2->hashName()}");
     }
 
+    public function testDeleteOldFiles()
+    {
+        $file1 = UploadedFile::fake()->create('video1.mp4')->size(1);
+        $file2 = UploadedFile::fake()->create('video2.mp4')->size(1);
+        $this->obj->uploadFiles([$file1, $file2]);
+        $this->obj->deleteOldFiles();
+        $this->assertCount(2, \Storage::allFiles());
+
+
+        $this->obj->oldFiles = [$file1->hashName()];
+        $this->obj->deleteOldFiles();
+        \Storage::assertMissing("1/{$file1->hashName()}");
+        \Storage::assertExists("1/{$file2->hashName()}");
+    }
+
     public function testDeleteFile()
     {
         $file = UploadedFile::fake()->create('video.mp4');
         $this->obj->uploadFile($file);
         $filename = $file->hashName();
-        $this->obj->deleteFile($file->hashName());
+        $this->obj->deleteFile($filename);
         \Storage::assertMissing("1/{$filename}");
 
         $file = UploadedFile::fake()->create('video.mp4');
@@ -60,6 +75,7 @@ class UploadFilesUnitTest extends TestCase
         $this->obj->deleteFiles([$file1->hashName(), $file2]);
         \Storage::assertMissing("1/{$file1->hashName()}");
         \Storage::assertMissing("1/{$file2->hashName()}");
-
     }
+
+
 }
